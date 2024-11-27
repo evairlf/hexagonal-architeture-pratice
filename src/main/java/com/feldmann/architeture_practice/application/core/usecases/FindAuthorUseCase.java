58ce813d3c738f826a01540d5cbb71eb96a370bf
Evairlf <evairleitefeldmann@gmail.com>
2024-11-27 14:35:41 -0300

    refactor(FindAuthorUseCase): added method find

--- src/main/java/com/feldmann/architeture_practice/application/core/usecases/FindAuthorUseCase.java
+++ src/main/java/com/feldmann/architeture_practice/application/core/usecases/FindAuthorUseCase.java
@@ -1,4 +1,20 @@
 package com.feldmann.architeture_practice.application.core.usecases;
 
-public class FindAuthorUseCase {
+import com.feldmann.architeture_practice.application.core.domain.Author;
+import com.feldmann.architeture_practice.application.ports.in.FindAuthorByIdInputPort;
+import com.feldmann.architeture_practice.application.ports.out.FindAuthorByIdOutputPort;
+
+public class FindAuthorUseCase implements FindAuthorByIdInputPort {
+
+    private final FindAuthorByIdOutputPort findAuthorByIdOutputPort;
+
+    public FindAuthorUseCase(FindAuthorByIdOutputPort findAuthorByIdOutputPort) {
+        this.findAuthorByIdOutputPort = findAuthorByIdOutputPort;
+    }
+
+
+    @Override
+    public Author find(Long id) {
+        return findAuthorByIdOutputPort.findById(id).orElseThrow(() -> new RuntimeException("Author not found!"));
+    }
 }
